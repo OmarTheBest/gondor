274f86a24c20138892f9ca0f39ba7ff47f94ada8
OmarTheBest <omarthebest@me.com>
2015-10-14 11:19:48 +0300

    SuperMegaUltraSoft

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 var znojmo=[10, 20, 30, 40, 50]
+var brno=[40, 20, 30, 10, 50]
+var x= Math.random()*100
+var y
 console.log (znojmo[3])
+if (x>znojmo[3])
+	{
+	console.log ('Hello!')
+	console.log (x)
+
+	}
+else
+	{
+	console.log ('Good Bye!')
+	console.log (x)
+	}
+MyFirstFnc(znojmo)
+MyFirstFnc(brno)
+
+function MyFirstFnc (MyInp)	
+{
+	for (y=0;y<MyInp.length;y++)
+	{
+	console.log (MyInp[y])
+
+	}
+}
